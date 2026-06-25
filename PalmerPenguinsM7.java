dadbe600f990e2f6bc945cb30de787b0fcb98cf2
bebejohnson <botswanajohnson@gmail.com>
2026-06-24 23:43:29 -0400

    Add files via upload

---
+++ PalmerPenguinsM7.java
@@ -0,0 +1,29 @@
+// PalmerPenguinsM7.java
+// Botswana Johnson
+// 06/24/2026
+// Refactored Palmer Penguins analysis using object-oriented approach
+
+public class PalmerPenguinsM7 {
+    public static void main(String[] args) {
+        // Create an instance of SpeciesCounter
+        SpeciesCounter counter = new SpeciesCounter();
+
+        // Read species data
+        counter.readSpeciesData();
+
+        // Initialize species count
+        counter.initializeSpeciesCount();
+
+        // Check if data is empty
+        if (counter.isDataEmpty()) {
+            System.out.println("Error: The file is empty or could not be read.");
+            return;
+        }
+
+        // Count species occurrences
+        counter.countSpecies();
+
+        // Print results
+        counter.printSpeciesCount();
+    }
+}
